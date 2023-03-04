8ac539207e263fdc1f71b2723d50be23c61951e5
moonwake769 <exhilaratedreverie288@protonmail.ch>
2023-03-04 16:01:38 +0100

    feat: initialized move_peace()

--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,6 @@
-use crate::piece::Color;
+use crate::piece::{Color, Piece, Position};
 
+#[derive(Debug, Clone)]
 pub struct Player {
     pub name: &'static str,
     pub pieces_color: Color,
@@ -8,5 +9,10 @@ pub struct Player {
 impl Player {
     pub fn new(_name: &'static str, _pieces_color: Color) -> Player {
         Player { name: _name, pieces_color: _pieces_color }
+    }
+
+    pub fn move_piece(self, _piece: &mut Piece, _position: Position) {
+        _piece.position.row = _position.row;
+        _piece.position.col = _position.col;
     }
 }
